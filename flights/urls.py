7e6b0d438442b7a586e965dd5c55d434a87cea39
박성준 <sungjunp1208@gmail.com>
2025-01-09 06:15:11 +0900

    feat: decision page 제작

--- flights/urls.py
+++ flights/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('cancellation/', views.flight_cancellation, name='flight_cancellation'),
     path('decision/', views.flight_decision, name='flight_decision'),
     path('decision_all/', views.flight_decision_all, name='flight_decision_all'),
-    path('decision_city', views.flight_decision_city, name='flight_decision_city'),
+    #path('decision_city', views.flight_decision_city, name='flight_decision_city'),
     path('decision_date', views.flight_decision_date, name='flight_decision_date'),
 ]
