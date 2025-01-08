40e1041932aa0a157e8fb0d781163cc1ef1a71bc
박성준 <sungjunp1208@gmail.com>
2025-01-09 06:54:06 +0900

    fix: home desighn

--- configuration/settings/base.py
+++ configuration/settings/base.py
@@ -104,7 +104,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
