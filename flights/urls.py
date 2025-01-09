4ac2091b9a38ba77c2c80432183ec44748015312
박성준 <sungjunp1208@gmail.com>
2025-01-09 21:37:51 +0900

    feat: add hotel research

--- flights/urls.py
+++ flights/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.flight_search, name='flight_search'),
+    path('hotels/', views.hotel_search, name='hotel_search'),
     path('details/', views.flight_details, name='flight_details'),
+    path('hotel_details/', views.hotel_details, name='hotel_details'),
     path('cancellation/', views.flight_cancellation, name='flight_cancellation'),
     path('decision/', views.flight_decision, name='flight_decision'),
     path('decision_all/', views.flight_decision_all, name='flight_decision_all'),
--- flights/views.py
+++ flights/views.py
@@ -10,6 +10,37 @@ def home(request):
 def flight_search(request):
     return render(request, 'flights/search.html')
 
+def hotel_search(request):
+    # 도쿄 명소 데이터
+    places_tokyo = [
+        {"name": "도쿄 타워", "image": "tokyo_tower.jpg"},
+        {"name": "도쿄 스카이트리", "image": "tokyo_skytree.jpg"},
+        {"name": "메이지 신궁", "image": "meiji_shrine.jpg"},
+        {"name": "센소지", "image": "sensoji.jpg"},
+        {"name": "신주쿠 교엔", "image": "shinjuku_garden.jpg"},
+    ]
+
+    # 다른 도시 데이터도 유사하게 구성
+    places_osaka = [
+        {"name": "도톤보리", "image": "dontonbori.jpg"},
+        {"name": "오사카 성", "image": "osaka_castle.jpg"},
+    ]
+    places_sapporo = [
+        {"name": "삿포로시 시계탑", "image": "clock_tower.jpg"},
+    ]
+    places_fukuoka = [
+        {"name": "오호리 공원", "image": "ohori_park.jpg"},
+    ]
+
+    # 템플릿으로 데이터 전달
+    context = {
+        "places_tokyo": places_tokyo,
+        "places_osaka": places_osaka,
+        "places_sapporo": places_sapporo,
+        "places_fukuoka": places_fukuoka,
+    }
+    return render(request, 'flights/hotel_search.html', context)
+
 def flight_details(request):
     # URL 파라미터 가져오기
     departure = request.GET.get('departure')
@@ -48,6 +79,10 @@ def flight_details(request):
     flights = flights.order_by(order)
 
     return render(request, 'flights/details.html', {'flights': flights, 'sort_by': sort_by})
+
+def hotel_details(request):
+    return render(request, 'flights/hotel_details.html')
+
 
 def flight_decision(request):
     return render(request, 'flights/decision.html')
